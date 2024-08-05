8cc65f09779b6c45f9bd64a513055b0dcfb91501
meutshuk <52316734+meutshuk@users.noreply.github.com>
2024-08-06 00:02:45 +1000

    init

---
+++ src/data/images.ts
@@ -0,0 +1,42 @@
+// src/data/image.js
+const IMAGES = [
+    {
+        name: 'Image 1',
+        description: 'Description for Image 1',
+        url: 'https://example.com/image1.jpg',
+        location: 'Location 1'
+    },
+    {
+        name: 'Image 2',
+        description: 'Description for Image 2',
+        url: 'https://example.com/image2.jpg',
+        location: 'Location 2'
+    },
+    {
+        name: 'Image 3',
+        description: 'Description for Image 3',
+        url: 'https://example.com/image3.jpg',
+        location: 'Location 3'
+    },
+    {
+        name: 'Image 1',
+        description: 'Description for Image 1',
+        url: 'https://example.com/image1.jpg',
+        location: 'Location 1'
+    },
+    {
+        name: 'Image 2',
+        description: 'Description for Image 2',
+        url: 'https://example.com/image2.jpg',
+        location: 'Location 2'
+    },
+    {
+        name: 'Image 3',
+        description: 'Description for Image 3',
+        url: 'https://example.com/image3.jpg',
+        location: 'Location 3'
+    },
+    // Add more image objects as needed
+];
+
+export default IMAGES;
